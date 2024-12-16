1a7b20f4e973283428afb7c78b0a990f5ba03f05
seanbarlisan <smbarlisan@gmail.com>
2024-12-16 16:10:49 -0500

    refactored code to utilize the new DB connection

--- password_manager/wifi.py
+++ password_manager/wifi.py
@@ -2,76 +2,103 @@ import sqlite3 # SQL DB to store information rather than a dictionary
 import json # will use in future to store all information as JSON files for the SQL DB
 from cryptography.fernet import Fernet # used to encrypt information on the device for usage
 
-class Credential:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
 credential_storage = dict()
+db_connection = sqlite3.Connection("credential_database.db")
+cur = db_connection.cursor()
+cur.execute("CREATE TABLE credential(username, password)")
 
 def create_credential():
-    new_credential = Credential()
-    username = input("Please input your username.")
-    while username is None and username != "exit":
-        username = input("Please input a valid credential, otherwise, input exit")
-        if username == "exit":
-            return
-    password = input("Please input your password.")
-    while password is None and password != "exit":
-        password = input("Please input a valid credential, otherwise, input exit")
-        if password == "exit":
-            return
-    # store the object into the dictionary
-    new_credential = Credential(username, password) # create the new object
-    credential_storage[username] = Credential(username, password) # store into the dictionary
+    try:
+        username = input("Please input your username:\n")
+        while username is None and username != "exit":
+            username = input("Please input a valid credential, otherwise, input exit")
+            if username == "exit":
+                return
+        password = input("Please input your password:\n")
+        while password is None and password != "exit":
+            password = input("Please input a valid credential, otherwise, input exit")
+            if password == "exit":
+                return
+        cur.execute("""
+            INSERT INTO credential VALUES
+                    (username, password) VALUES (?, ?)
+            """, (username, password))
+        # Include encrypt option here
+        db_connection.commit()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+    finally:
+        db_connection.close()
     return 
 
 def delete_credential():
-    remove_username = input("What credential would you like to remove?")
-    while remove_username is None and remove_username != "exit":
-        remove_username = input("Please input a valid credential, otherwise, input exit")
-        if remove_username == "exit":
-            return
-    del credential_storage(remove_username) # Deletes the credential from the database
-    return
+    try:
+        remove_username = input("What credential would you like to remove?\n")
+        while remove_username is None and remove_username != "exit":
+            remove_username = input("Please input a valid credential, otherwise, input exit\n")
+            if remove_username == "exit":
+                return
+        cur.execute("""
+            DELETE FROM credential WHERE username = ?;
+            """, (remove_username,))
+        db_connection.commit()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+    finally:
+        db_connection.close()
 
 def view_credential():
-    view_value = input("What credential would you like to view?")
-    while view_value is None and view_value != "exit":
-        view_value = input("Please input a valid credential, otherwise, input exit")
-        if view_value == "exit":
-            return
-    print(credential_storage[view_value].values()) # Prints out the credential KEY and credential VALUE
-    return
+    try:
+        view_value = input("What credential would you like to view?\n")
+        while view_value is None and view_value != "exit":
+            view_value = input("Please input a valid credential, otherwise, input exit\n")
+            if view_value == "exit":
+                return
+        cur.execute("""
+            SELECT username, password FROM credentials WHERE username = ?;
+        """, (view_value,))
+        row = cur.fetchone()
+        if row:
+            print(f"Username: {row[0]}, Password: {row[1]}")
+        else:
+            print(f"No credentials found for username '{view_value}'.")
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return None
+    finally:
+        # Close the connection
+        db_connection.close()
 
 def edit_credential():
-    edit_value - input("What credential would you like to edit?")
+    edit_value - input("What credential would you like to edit?\n")
     while edit_value is None and edit_value != "exit":
-        edit_value = input("Please input a valid credential, otherwise, input exit")
+        edit_value = input("Please input a valid credential, otherwise, input exit\n")
         if edit_value == "exit":
             return
-    credential_storage[edit_value] = input("What would you like your new password to be?")
+    credential_storage[edit_value] = input("What would you like your new password to be?\n")
     return
 
-def encrypt_credential(password):
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-    encPassword = fernet.encrypt(password.encode())
-    return encPassword
-
-def decrypt_credential(password): # how will we decrypt this password from function?
-    decPassword = fernet.decrypt(password.decode())
-    return
+# Implement a function to encrypt and decrypt the password values we create, should use the fermet library
 
 def main():
-    choice = input("What would you like to do?")
-    match choice:
-        case("1"):
-            create_credential()
-        case("2"):
-            delete_credential()
-        case("3"):
-            view_credential()
-        case("4"):
-            edit_credential()
+    while True:
+        choice = input("What would you like to do?\n")
+        choice = choice.lower()
+        match choice:
+            case("1"):
+                create_credential()
+            case("2"):
+                delete_credential()
+            case("3"):
+                view_credential()
+            case("4"):
+                edit_credential()
+            case("exit"):
+                return
+            case _:
+                print("Please input a option or type 'exit' to leave.\n")
+
     return
+
+if __name__ == "__main__":
+    main()
